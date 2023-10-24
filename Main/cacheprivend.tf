20e2cf5841326b0b07054215386403862ff0827c
LeRon Ellis <lojack2525@gmail.com>
2023-10-24 14:43:18 -0700

    adding comments

--- Main/cacheprivend.tf
+++ Main/cacheprivend.tf
@@ -1,11 +1,12 @@
-#Create Private Endpoint for Azure Cache for Redis
-
+#Create Private Endpoint for Azure Cache for Redisv
 resource "azurerm_private_endpoint" "cacheprivend" {
   name                = "sillk-cachepep"
   location            = azurerm_resource_group.SiLLK.location
   resource_group_name = azurerm_resource_group.SiLLK.name
   subnet_id           = azurerm_subnet.endpoint-subnet.id
 
+  
+  #Create private service connection
   private_service_connection {
     name                           = "lepe1-privateserviceconnection"
     private_connection_resource_id = azurerm_redis_cache.cache.id
@@ -13,17 +14,20 @@ resource "azurerm_private_endpoint" "cacheprivend" {
     is_manual_connection           = false
   }
 
+#create private dns group
   private_dns_zone_group {
     name                 = "cacheprivend-dns-zone-group"
     private_dns_zone_ids = [azurerm_private_dns_zone.cacheprivend.id]
   }
 }
 
+#create private dns zone
 resource "azurerm_private_dns_zone" "cacheprivend" {
   name                = "privatelink.redis.cache.windows.net"
   resource_group_name = azurerm_resource_group.SiLLK.name
 }
 
+#create private network link
 resource "azurerm_private_dns_zone_virtual_network_link" "cacheprivend" {
   name                  = "sillkcache-link"
   resource_group_name   = azurerm_resource_group.SiLLK.name
